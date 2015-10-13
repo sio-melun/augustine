2f7e1eec39ed88ec75217addc4ebbdf98f0bd600
Thibaut-Comte <thibaut.comte.1@gmail.com>
2015-10-13 11:53:33 +0200

    integration sans utilisation du bundle FOSUser

--- src/Augustine/PlatformBundle/Entity/User.php
+++ src/Augustine/PlatformBundle/Entity/User.php
@@ -1,7 +1,7 @@
 <?php
 // src/AugustinePlatformBundle/Entity/User.php
 
-namespace AugustinePlatformBundle\Entity;
+namespace Augustine\PlatformBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
